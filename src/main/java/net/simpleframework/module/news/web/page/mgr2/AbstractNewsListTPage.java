834a19a3e6df9596d49fc4b9016e82ba7a5232ff
CHENK <CHENK@CHENK-PC>
2015-12-17 11:59:29 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

---
+++ src/main/java/net/simpleframework/module/news/web/page/mgr2/AbstractNewsListTPage.java
@@ -0,0 +1,14 @@
+package net.simpleframework.module.news.web.page.mgr2;
+
+import net.simpleframework.module.common.web.page.AbstractMgrTPage;
+import net.simpleframework.module.news.INewsContextAware;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public abstract class AbstractNewsListTPage extends AbstractMgrTPage implements INewsContextAware {
+
+}
