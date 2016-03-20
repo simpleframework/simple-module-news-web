925775bdb0b80533f1de516eb8eee05c6f12b30c
CHENK <CHENK@CHENK-PC>
2016-03-20 15:15:40 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/news/web/page/NewsCommentHandler.java
+++ src/main/java/net/simpleframework/module/news/web/page/NewsCommentHandler.java
@@ -20,7 +20,8 @@ import net.simpleframework.mvc.component.ext.comments.ctx.CommentCtxHandler;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class NewsCommentHandler extends CommentCtxHandler<NewsComment> implements INewsContextAware {
