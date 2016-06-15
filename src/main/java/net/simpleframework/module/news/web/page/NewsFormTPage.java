8e5d778347d2a44f7c6a9d3ee5e060377299c203
CHENK <CHENK@CHENK-PC>
2016-06-15 20:08:30 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/news/web/page/NewsFormTPage.java
+++ src/main/java/net/simpleframework/module/news/web/page/NewsFormTPage.java
@@ -49,6 +49,7 @@ import net.simpleframework.mvc.common.element.TableRows;
 import net.simpleframework.mvc.common.element.TextButton;
 import net.simpleframework.mvc.component.AbstractComponentBean;
 import net.simpleframework.mvc.component.ComponentParameter;
+import net.simpleframework.mvc.component.base.ajaxrequest.AjaxRequestBean;
 import net.simpleframework.mvc.component.base.validation.EValidatorMethod;
 import net.simpleframework.mvc.component.base.validation.Validator;
 import net.simpleframework.mvc.component.ext.attachments.AttachmentBean;
@@ -447,9 +448,16 @@ public class NewsFormTPage extends FormTableRowTemplatePage implements INewsCont
 		if (status == EContentStatus.edit) {
 			el.add(NewsUtils.createStatusAct(pp, EContentStatus.publish, news));
 		} else if (status == EContentStatus.audit) {
-			el.add(new ButtonElement($m("NewsFormTPage.21")).setHighlight(true));
+			final AjaxRequestBean ajaxRequest = addAjaxRequest(pp, "NewsFormTPage_auditPage",
+					AuditEditPage.class);
+			addWindowBean(pp, "NewsFormTPage_auditWin", ajaxRequest).setTitle($m("NewsFormTPage.23"))
+					.setWidth(420).setHeight(240);
+
+			el.add(new ButtonElement($m("NewsFormTPage.21")).setHighlight(true).setOnclick(
+					"$Actions['NewsFormTPage_auditWin']('newsId=" + news.getId() + "&bpass=true');"));
 			el.add(SpanElement.SPACE);
-			el.add(new ButtonElement($m("NewsFormTPage.22")));
+			el.add(new ButtonElement($m("NewsFormTPage.22"))
+					.setOnclick("$Actions['NewsFormTPage_auditWin']('newsId=" + news.getId() + "');"));
 		} else if (status == EContentStatus.publish) {
 			el.add(NewsUtils.createStatusAct(pp, EContentStatus.edit, news)
 					.setText($m("NewsMgrPage.7")).setHighlight(true));
@@ -529,6 +537,35 @@ public class NewsFormTPage extends FormTableRowTemplatePage implements INewsCont
 			sb.append("if (saveBtn) { Event.simulate(saveBtn, 'click'); }");
 			sb.append("else { $Actions.reloc(); }");
 			return sb.toString();
+		}
+	}
+
+	public static class AuditEditPage extends FormTableRowTemplatePage {
+		@Override
+		protected void onForward(final PageParameter pp) throws Exception {
+			super.onForward(pp);
+
+			addFormValidationBean(pp).addValidators(
+					new Validator(EValidatorMethod.required, "#ae_ccomment"));
+		}
+
+		@Override
+		public JavascriptForward onSave(final ComponentParameter cp) throws Exception {
+			// final News news = NewsUtils.getNews(cp);
+			return new JavascriptForward("$Actions.reloc();");
+		}
+
+		@Override
+		protected TableRows getTableRows(final PageParameter pp) {
+			final boolean bpass = pp.getBoolParameter("bpass");
+			final InputElement ae_bpass = InputElement.hidden("ae_bpass").setVal(bpass);
+			final InputElement ae_ccomment = InputElement.textarea("ae_ccomment").setRows(4);
+
+			final TableRow r1 = new TableRow(new RowField($m(bpass ? "NewsFormTPage.24"
+					: "NewsFormTPage.25"), ae_bpass, ae_ccomment));
+			final TableRow r2 = new TableRow(new RowField($m("NewsFormTPage.26"), InputElement
+					.textarea().setReadonly(true)));
+			return TableRows.of(r1, r2);
 		}
 	}
 
